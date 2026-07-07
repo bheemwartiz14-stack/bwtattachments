be3ceb91c9f2fe0f0267c0aec411c2f7134a7d6e
bheemwartiz14-stack <bheem.wartiz14@gmail.com>
2026-07-07 15:09:52 +0530

    fix: Add jQuery script inclusion to base layout for enhanced functionality

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -40,6 +40,8 @@
 
 <body {{ $attributes }}>
     {{ $slot }}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
     @livewireScripts
     @stack('scripts')
 </body>
