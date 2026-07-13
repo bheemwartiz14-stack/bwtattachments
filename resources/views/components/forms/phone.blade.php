88076e207f79e7f63417f84734cdb00e6e40f260
amit davre <amitdavre7@gmail.com>
2026-07-13 17:05:41 +0530

    fix: Remove input formatting logic from phone input component

--- resources/views/components/forms/phone.blade.php
+++ resources/views/components/forms/phone.blade.php
@@ -21,16 +21,5 @@
     :disabled="$disabled"
     :error="$error"
     :hint="$hint"
-    x-data
-    x-on:input="
-        let digits = $event.target.value.replace(/\D/g, '');
-        if (digits.length > 11) digits = digits.slice(0, 11);
-        let formatted = '';
-        if (digits.length > 0) formatted = '+' + digits[0];
-        if (digits.length > 1) formatted += ' (' + digits.slice(1, 4);
-        if (digits.length > 4) formatted += ') ' + digits.slice(4, 7);
-        if (digits.length > 7) formatted += '-' + digits.slice(7);
-        $event.target.value = formatted;
-    "
     {{ $attributes }}
 />
