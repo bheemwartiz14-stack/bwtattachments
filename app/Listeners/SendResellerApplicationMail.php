ccef6083219ba2482f62d5f2275597937a259e42
bheemwartiz14-stack <bheem.wartiz14@gmail.com>
2026-07-23 12:17:44 +0530

    uPDATED CODE

--- app/Listeners/SendResellerApplicationMail.php
+++ app/Listeners/SendResellerApplicationMail.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Listeners;
@@ -6,12 +7,14 @@ namespace App\Listeners;
 use App\Events\ResellerApplicationSubmitted;
 use App\Mail\ResellerApplicationAcknowledgementMail;
 use App\Mail\ResellerApplicationMail;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class SendResellerApplicationMail
 {
     public function handle(ResellerApplicationSubmitted $event): void
     {
+        Log::info("EMAIL SENT TO " . config('mail.from.admin_email'));
         Mail::to(config('mail.from.admin_email'))->send(
             new ResellerApplicationMail($event->application)
         );
