8983c0a0d42613db8e625cde2afe6e2df0e2496e
Bheem Singh <bheem.wartiz14@gmail.com>
2026-08-06 14:59:41 +0530

    Fixed the bug issue

--- routes/console.php
+++ routes/console.php
@@ -7,7 +7,3 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Schedule::command('media-library:clean')->hourly();
-
-Schedule::command('media-library:regenerate')->hourly();
